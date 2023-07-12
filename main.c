6febcdcf33b25c725d9740dd0ed11063cc5c50d9
Matthew Daly <matthewd673@outlook.com>
2023-07-12 18:20:21 -0400

    Add NFAModule, concat & union modules

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[]) {
     printf("trex - tiny regular expression engine\n");
     RegEx re = compile("abc");
-    printf("Match 'abc'?\t%d\n", match(re, "abc"));
-    printf("Match 'ab'?\t%d\n", match(re, "ab"));
-    printf("Match 'abcd'?\t%d\n", match(re, "abcd"));
-    printf("Match '0abc'?\t%d\n", match(re, "0abc"));
-    printf("Match 'ganbddc\t%d\n", match(re, "ganbddc"));
+    // printf("Match 'abc'?\t%d\n", match(re, "abc"));
+    // printf("Match 'ab'?\t%d\n", match(re, "ab"));
+    // printf("Match 'abcd'?\t%d\n", match(re, "abcd"));
+    // printf("Match '0abc'?\t%d\n", match(re, "0abc"));
+    // printf("Match 'ganbddc\t%d\n", match(re, "ganbddc"));
 }
--- regex.c
+++ regex.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
-#include "dfa.h"
+#include "nfa.h"
 #include "regex.h"
 
 struct RegEx {
     DFAState entry;
+};
+
+typedef struct NFAModule *NFAModule;
+struct NFAModule {
+    List exits;
+    NFAState entry;
 };
 
 RegEx new_RegEx(DFAState entry) {
@@ -16,27 +22,88 @@ RegEx new_RegEx(DFAState entry) {
     return new;
 }
 
+NFAModule new_NFAModule() {
+    NFAModule new = (NFAModule)malloc(sizeof(struct NFAModule));
+
+    new->exits = new_List();
+
+    return new;
+}
+
+void connectModules(NFAModule previous, NFAModule next) {
+    Node current = List_getHead(previous->exits);
+    while (current != NULL) {
+        NFAState state = (NFAState)List_getObject(current);
+        NFAState_addTransition(state, next->entry, 0);
+        NFAState_setSuccess(state, 0);  // once connected, exits no longer succeed
+        current = List_getNext(current);
+    }
+}
+
+void subsumeModuleExits(NFAModule this, NFAModule child) {
+    Node current = List_getHead(child->exits);
+    while (current != NULL) {
+        List_add(this->exits, List_getObject(current));
+        current = List_getNext(current);
+    }
+}
+
+NFAModule moduleConcatenation(NFAModule previous, char c) {
+    // create new module and connect it to previous
+    NFAModule this = new_NFAModule();
+    this->entry = new_NFAState();
+    connectModules(previous, this);
+
+    // transition from entry to exit on char
+    NFAState exit = new_NFAState();
+    NFAState_setSuccess(exit, 1); // exit is always assumed to succeed
+    NFAState_addTransition(this->entry, exit, c);
+
+    // add exit to module
+    List_add(this->exits, exit);
+
+    return this;
+}
+
+NFAModule moduleUnion(NFAModule previous, NFAModule left, NFAModule right) {
+    // create new module and connect to previous
+    NFAModule this = new_NFAModule();
+    this->entry = new_NFAState();
+    connectModules(previous, this);
+
+    // create epsilon transitions from entry to branches' entries
+    NFAState_addTransition(this->entry, left->entry, 0);
+    NFAState_addTransition(this->entry, right->entry, 0);
+
+    // make branches' exits our own
+    subsumeModuleExits(this, left);
+    subsumeModuleExits(this, right);
+
+    return this;
+}
+
+// NFAState moduleKClosure(NFAModule closure) {
+//     //
+// }
+
 RegEx compile(char *expression) {
     int i = 0;
 
-    DFAState entry = new_DFAState();
-    DFAState lastState = entry;
+    NFAState entry = new_NFAState();
+    NFAState lastState = entry;
 
-    // TODO: temp
+    // TODO: temp, bare minimum
     while (expression[i] != 0) {
         // create new state and transition to it
-        DFAState newState = new_DFAState();
-        DFAState_addTransition(lastState, newState, expression[i]);
+        NFAState newState = new_NFAState();
+        NFAState_addTransition(lastState, newState, expression[i]);
         lastState = newState;
         i++;
     }
     // mark last node as success
-    DFAState_setSuccess(lastState, 1);
+    NFAState_setSuccess(lastState, 1);
 
-    // for debugging:
-    DFAState_print(entry);
-
-    return new_RegEx(entry);
+    return NULL;
 }
 
 int match(RegEx re, char *str) {
